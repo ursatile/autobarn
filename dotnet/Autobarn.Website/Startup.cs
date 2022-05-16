a7ec612e72d46defc06f69e4a44c27b9c2587ec0
Dylan Beattoie <dylan@dylanbeattie.net>
2022-05-16 16:59:36 +0100

    Update GraphQL dependencies to 5.x

--- dotnet/Autobarn.Website/Startup.cs
+++ dotnet/Autobarn.Website/Startup.cs
@@ -1,68 +1,75 @@
 using Autobarn.Data;
 using Autobarn.Website.GraphQL.Schemas;
 using GraphiQl;
-using GraphQL.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Autobarn.Website.GraphQL.GraphTypes;
+using GraphQL;
+using GraphQL.MicrosoftDI;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Server;
+using GraphQL.Types;
 
 namespace Autobarn.Website {
-	public class Startup {
-		protected virtual string DatabaseMode => Configuration["DatabaseMode"];
+    public class Startup {
+        protected virtual string DatabaseMode => Configuration["DatabaseMode"];
 
-		public Startup(IConfiguration configuration) {
-			Configuration = configuration;
-		}
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services) {
-			services.AddRouting(options => options.LowercaseUrls = true);
-			services.AddControllersWithViews().AddNewtonsoftJson();
-			services.AddRazorPages().AddRazorRuntimeCompilation();
-			Console.WriteLine(DatabaseMode);
-			switch (DatabaseMode) {
-				case "sql":
-					var sqlConnectionString = Configuration.GetConnectionString("AutobarnSqlConnectionString");
-					services.UseAutobarnSqlDatabase(sqlConnectionString);
-					break;
-				default:
-					services.AddSingleton<IAutobarnDatabase, AutobarnCsvFileDatabase>();
-					break;
-			}
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddControllersWithViews().AddNewtonsoftJson();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            Console.WriteLine(DatabaseMode);
+            switch (DatabaseMode) {
+                case "sql":
+                    var sqlConnectionString = Configuration.GetConnectionString("AutobarnSqlConnectionString");
+                    services.UseAutobarnSqlDatabase(sqlConnectionString);
+                    break;
+                default:
+                    services.AddSingleton<IAutobarnDatabase, AutobarnCsvFileDatabase>();
+                    break;
+            }
+            services.AddGraphQL(builder => builder
+                .AddHttpMiddleware<ISchema>()
+                .AddNewtonsoftJson()
+                .AddSchema<AutobarnSchema>()
+                .AddGraphTypes(typeof(VehicleGraphType).Assembly)
+            );
 
-			services.AddScoped<AutobarnSchema>();
-			services
-				.AddGraphQL(options => options.EnableMetrics = false)
-				.AddNewtonsoftJson();
-		}
+        }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
-				app.UseDeveloperExceptionPage();
-			} else {
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-			app.UseHttpsRedirection();
-			app.UseDefaultFiles();
-			app.UseStaticFiles();
-			app.UseRouting();
-			app.UseAuthorization();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
 
-			app.UseGraphQL<AutobarnSchema>();
-			app.UseGraphiQl("/graphiql");
+            app.UseGraphQL<ISchema>();
+            app.UseGraphiQl("/graphiql");
 
-			app.UseEndpoints(endpoints => {
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-			});
-		}
-	}
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
 }
