0d1edf90d77c1ad920c13e77f9d82d3e30f1338e
Dylan Beattie <dylan@dylanbeattie.net>
2023-01-22 22:14:33 +0000

    Finished HTTP APIs

--- dotnet/Autobarn.Website/Controllers/Api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/Api/VehiclesController.cs
@@ -1,5 +1,6 @@
-﻿using Autobarn.Data;
+using Autobarn.Data;
 using Autobarn.Data.Entities;
+using Autobarn.Website.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -16,5 +17,50 @@ namespace Autobarn.Website.Controllers.Api {
 		// GET: api/vehicles
 		[HttpGet]
 		public IEnumerable<Vehicle> Get() => db.ListVehicles();
+
+		// GET api/vehicles/ABC123
+		[HttpGet("{id}")]
+		public IActionResult Get(string id) {
+			var vehicle = db.FindVehicle(id);
+			if (vehicle == default) return NotFound();
+			return Ok(vehicle);
+		}
+
+		// POST api/vehicles
+		[HttpPost]
+		public IActionResult Post([FromBody] VehicleDto dto) {
+			var vehicleModel = db.FindModel(dto.ModelCode);
+			var vehicle = new Vehicle {
+				Registration = dto.Registration,
+				Color = dto.Color,
+				Year = dto.Year,
+				VehicleModel = vehicleModel
+			};
+			db.CreateVehicle(vehicle);
+			return Ok(vehicle);
+		}
+
+		// PUT api/vehicles/ABC123
+		[HttpPut("{id}")]
+		public IActionResult Put(string id, [FromBody] VehicleDto dto) {
+			var vehicleModel = db.FindModel(dto.ModelCode);
+			var vehicle = new Vehicle {
+				Registration = dto.Registration,
+				Color = dto.Color,
+				Year = dto.Year,
+				ModelCode = vehicleModel.Code
+			};
+			db.UpdateVehicle(vehicle);
+			return Ok(dto);
+		}
+
+		// DELETE api/vehicles/ABC123
+		[HttpDelete("{id}")]
+		public IActionResult Delete(string id) {
+			var vehicle = db.FindVehicle(id);
+			if (vehicle == default) return NotFound();
+			db.DeleteVehicle(vehicle);
+			return NoContent();
+		}
 	}
 }
